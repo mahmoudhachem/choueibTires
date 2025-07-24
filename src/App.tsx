49292a3d1bf80fb1c195d9a496c9509492937fef
mahmoudhachem <hachemahmoud@outlook.com>
2025-07-24 17:36:51 +0300

    whatsapp

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const buttons = [
   },
   {
     label: "WhatsApp",
-    href: "https://wa.me/0096171584754",
+    href: "https://wa.me/96171584754",
     color: "bg-green-500",
     icon: (
       <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24">
